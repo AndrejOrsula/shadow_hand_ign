baac195cfd2231a747686a8d9a32a7072ce15431
Andrej Orsula <orsula.andrej@gmail.com>
2021-06-12 00:53:45 +0200

    Initial commit
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ scripts/estimate_inertial_properties.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import sys
+from os import path, listdir
+
+import trimesh
+from pcg_gazebo.parsers.sdf import SDF, create_sdf_element
+# Note: Both `trimesh` and `pcg_gazebo` can be installed via `pip`
+# `trimesh` is used to estimate volume and inertial properties from meshes of links
+# `pcg_gazebo` is used for its SDF parser
+
+# Warning: This script might fail with the current material
+
+def main():
+
+    # Total mass taken from datasheet (given as ~4.2kg)
+    # You can also use your own estimate of total mass if you managed to weigh Shadow Hand yourself :)
+    total_mass = 4.2
+    if len(sys.argv) > 1:
+        if float(sys.argv[1]) > 0.0:
+            total_mass = float(sys.argv[1])
+        else:
+            print("Error: Total mass of Shadow Hand (first argument) must be positive.")
+            exit(1)
+    print('Estimating inertial properties for each link to add up to %f kg' % total_mass)
+
+    # Get path to all visual meshes
+    visual_mesh_dir = path.join(path.dirname(path.dirname(
+        path.realpath(__file__))), 'shadow_hand', 'meshes', 'visual')
+    visual_mesh_basenames = listdir(visual_mesh_dir)
+    visual_mesh_basenames.sort()
+
+    # Load all meshes
+    meshes = {}
+    for mesh_basename in visual_mesh_basenames:
+        link_name = path.splitext(mesh_basename)[0]
+        mesh_path = path.join(visual_mesh_dir, mesh_basename)
+        meshes[link_name] = trimesh.load(mesh_path,
+                                         force='mesh',
+                                         ignore_materials=True)
+
+    # Compute the total volume of the hand in order to estimate the required density
+    total_volume = 0.0
+    for link_name in meshes:
+        mesh = meshes[link_name]
+        print('Volume estimate of %s: %f m^3' % (link_name, mesh.volume))
+        if 'finger' in link_name or 'knuckle' in link_name:
+            total_volume += 4*mesh.volume
+            print(f'Note: {link_name} volume added four times to the total volume')
+        else:
+            total_volume += mesh.volume
+
+    # Compute average density
+    average_density = total_mass/total_volume
+    print('Average density estimate: %f kg/m^3' % average_density)
+
+    # Estimate inertial properties for each link
+    mass = {}
+    inertia = {}
+    centre_of_mass = {}
+    for link_name in meshes:
+        mesh = meshes[link_name]
+        mesh.density = average_density
+        mass[link_name] = mesh.mass
+        inertia[link_name] = mesh.moment_inertia
+        centre_of_mass[link_name] = mesh.center_mass
+
+    # Create a new SDF with one model
+    sdf = SDF()
+    sdf.add_model(name='shadow_hand')
+    model = sdf.models[0]
+
+    # Set inertial properties for each link into the SDF
+    for link_name in meshes:
+        link = create_sdf_element('link')
+        link.mass = mass[link_name]
+        link.inertia.ixx = inertia[link_name][0][0]
+        link.inertia.iyy = inertia[link_name][1][1]
+        link.inertia.izz = inertia[link_name][2][2]
+        link.inertia.ixy = inertia[link_name][0][1]
+        link.inertia.ixz = inertia[link_name][0][2]
+        link.inertia.iyz = inertia[link_name][1][2]
+        link.inertial.pose = [centre_of_mass[link_name][0],
+                              centre_of_mass[link_name][1],
+                              centre_of_mass[link_name][2],
+                              0.0, 0.0, 0.0]
+        model.add_link(link_name, link)
+
+    # Write into output file
+    output_file = 'shadow_hand_inertial_out.sdf'
+    sdf.export_xml(output_file)
+    print('Results written into "%s"' % output_file)
+
+
+if __name__ == "__main__":
+    main()
